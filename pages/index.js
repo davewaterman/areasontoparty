87d382d54589945544766118787ff23d8aa3b4f4
Dave Waterman <waterman_david@bah.com>
2021-07-20 16:15:10 -0400

    fixed bg issue on mobile

--- pages/index.js
+++ pages/index.js
@@ -149,7 +149,9 @@ class FlipClock extends React.Component {
 
     return (
       <div
-        className={"flipClock text-right space-x-2 sm:space-x-5 lg:space-x-10"}
+        className={
+          "flipClock w-11/12 text-right space-x-2 sm:w-auto sm:space-x-5 lg:space-x-10"
+        }
       >
         <FlipUnitContainer unit={"Days"} digit={days} shuffle={daysShuffle} />
         <FlipUnitContainer
@@ -175,18 +177,17 @@ class FlipClock extends React.Component {
 // function component
 const Header = ({ pageHeader }) => {
   return (
-    <header className="my-8 text-4xl text-center bg-black">
-      <h1>{pageHeader}</h1>
+    <header className="my-8 text-4xl text-center bg-black w-5/6 lg:w-1/2">
+      <h1 className="text-center">{pageHeader}</h1>
     </header>
   );
 };
 
 const Form = ({ textboxHeader, textboxBodyText, buttonText }) => {
-  console.log(documentToReactComponents(textboxBodyText));
   return (
     <section
       id="dw_mailchimp_form"
-      className="my-8 backdrop-filter backdrop-grayscale backdrop-blur-sm backdrop-opacity-80 rounded-2xl"
+      className="w-5/6 my-8 backdrop-filter backdrop-grayscale backdrop-blur-sm backdrop-opacity-80 rounded-2xl"
     >
       <p className="text-white font-mono text-center m-5 text-xl">
         {textboxHeader}
@@ -201,7 +202,7 @@ const Form = ({ textboxHeader, textboxBodyText, buttonText }) => {
               type="email"
               defaultValue=""
               name="EMAIL"
-              className="required email font-mono p-1 text-sm"
+              className="required email font-mono p-1 text-sm bg-white"
               id="mce-EMAIL"
               placeholder="Enter your email"
             />
@@ -218,7 +219,7 @@ const Form = ({ textboxHeader, textboxBodyText, buttonText }) => {
                 value={buttonText}
                 name="subscribe"
                 id="mc-embedded-subscribe"
-                className="button font-mono px-2 mt-3"
+                className="button font-mono px-2 mt-3 bg-white"
               />
             </div>
           </div>
@@ -243,17 +244,17 @@ const Form = ({ textboxHeader, textboxBodyText, buttonText }) => {
 // function component
 const App = ({ content }) => {
   return (
-    <div id="app">
+    <div id="app" className="flex items-center">
       <Image
         src={"/crowd.jpg"}
         alt="Crowd"
         layout="fill"
         objectFit="cover"
         objectPosition="cover"
-        className="z-0"
+        className="z-0 w-screen"
         unoptimized="true"
       />
-      <div className="flex flex-col justify-center items-center z-10">
+      <div className="flex flex-col justify-center items-center z-10 w-screen">
         <Header pageHeader={content.pageHeader} />
         <FlipClock countdownDate={content.countdownDate} />
         <Form
@@ -261,169 +262,169 @@ const App = ({ content }) => {
           textboxBodyText={content.textboxBodyText}
           buttonText={content.buttonText}
         />
-      </div>
-      <style global jsx>{`
-        @import url("https://fonts.googleapis.com/css?family=Droid+Sans+Mono");
-        #app * {
-          box-sizing: border-box;
-        }
-
-        #app body {
-          margin: 0;
-        }
-
-        #app {
-          display: flex;
-          position: relative;
-          width: 100%;
-          min-height: 100vh;
-          justify-content: center;
-          align-items: center;
-        }
-
-        #app header {
-          display: flex;
-          position: relative;
-        }
-        #app header h1 {
-          font-family: "Droid Sans Mono", monospace;
-          font-weight: lighter;
-          text-transform: uppercase;
-          letter-spacing: 0.1em;
-          color: white;
-        }
-
-        #app .flipClock {
-          display: flex;
-          //justify-content: space-between;
-          //width: 560px;
-        }
-
-        #app .flipUnitContainer {
-          display: block;
-          position: relative;
-          //width: 90px;
-          height: 70px;
-          perspective-origin: 50% 50%;
-          perspective: 300px;
-          background-color: white;
-          border-radius: 3px;
-          box-shadow: 0px 10px 10px -10px grey;
-        }
-
-        #app .upperCard,
-        #app .lowerCard {
-          display: flex;
-          position: relative;
-          justify-content: center;
-          width: 100%;
-          height: 50%;
-          overflow: hidden;
-          border: 1px solid whitesmoke;
-        }
-        #app .upperCard span,
-        #app .lowerCard span {
-          font-size: 5em;
-          font-family: "Droid Sans Mono", monospace;
-          font-weight: lighter;
-          color: #333333;
-        }
-
-        #app .upperCard {
-          align-items: flex-end;
-          border-bottom: 0.5px solid whitesmoke;
-          border-top-left-radius: 3px;
-          border-top-right-radius: 3px;
-        }
-        #app .upperCard span {
-          transform: translateY(50%);
-        }
-
-        #app .lowerCard {
-          align-items: flex-start;
-          border-top: 0.5px solid whitesmoke;
-          border-bottom-left-radius: 3px;
-          border-bottom-right-radius: 3px;
-        }
-        #app .lowerCard span {
-          transform: translateY(-50%);
-        }
-
-        #app .flipCard {
-          display: flex;
-          justify-content: center;
-          position: absolute;
-          left: 0;
-          width: 100%;
-          height: 50%;
-          overflow: hidden;
-          backface-visibility: hidden;
-        }
-        #app .flipCard span {
-          font-family: "Droid Sans Mono", monospace;
-          font-size: 5em;
-          font-weight: lighter;
-          color: #333333;
-        }
-        #app .flipCard.unfold {
-          top: 50%;
-          align-items: flex-start;
-          transform-origin: 50% 0%;
-          transform: rotateX(180deg);
-          background-color: white;
-          border-bottom-left-radius: 3px;
-          border-bottom-right-radius: 3px;
-          border: 0.5px solid whitesmoke;
-          border-top: 0.5px solid whitesmoke;
-        }
-        #app .flipCard.unfold span {
-          transform: translateY(-50%);
-        }
-        #app .flipCard.fold {
-          top: 0%;
-          align-items: flex-end;
-          transform-origin: 50% 100%;
-          transform: rotateX(0deg);
-          background-color: white;
-          border-top-left-radius: 3px;
-          border-top-right-radius: 3px;
-          border: 0.5px solid whitesmoke;
-          border-bottom: 0.5px solid whitesmoke;
-        }
-        #app .flipCard.fold span {
-          transform: translateY(50%);
-        }
-
-        #app .fold {
-          animation: fold 0.6s cubic-bezier(0.455, 0.03, 0.515, 0.955) 0s 1
-            normal forwards;
-          transform-style: preserve-3d;
-        }
-
-        #app .unfold {
-          animation: unfold 0.6s cubic-bezier(0.455, 0.03, 0.515, 0.955) 0s 1
-            normal forwards;
-          transform-style: preserve-3d;
-        }
-
-        @keyframes fold {
-          0% {
-            transform: rotateX(0deg);
+        <style global jsx>{`
+          @import url("https://fonts.googleapis.com/css?family=Droid+Sans+Mono");
+          #app * {
+            box-sizing: border-box;
           }
-          100% {
-            transform: rotateX(-180deg);
-          }
-        }
 
-        @keyframes unfold {
-          0% {
+          #app body {
+            margin: 0;
+          }
+
+          #app {
+            display: flex;
+            position: relative;
+            width: 100%;
+            min-height: 100vh;
+            justify-content: center;
+            align-items: center;
+          }
+
+          #app header {
+            display: flex;
+            position: relative;
+          }
+          #app header h1 {
+            font-family: "Droid Sans Mono", monospace;
+            font-weight: lighter;
+            text-transform: uppercase;
+            letter-spacing: 0.1em;
+            color: white;
+          }
+
+          .flipClock {
+            display: flex;
+            //justify-content: space-between;
+            //width: 560px;
+          }
+
+          .flipUnitContainer {
+            display: block;
+            position: relative;
+            //width: 90px;
+            height: 70px;
+            perspective-origin: 50% 50%;
+            perspective: 300px;
+            background-color: white;
+            border-radius: 3px;
+            box-shadow: 0px 10px 10px -10px grey;
+          }
+
+          .upperCard,
+          .lowerCard {
+            display: flex;
+            position: relative;
+            justify-content: center;
+            width: 100%;
+            height: 50%;
+            overflow: hidden;
+            border: 1px solid whitesmoke;
+          }
+          .upperCard span,
+          .lowerCard span {
+            font-size: 5em;
+            font-family: "Droid Sans Mono", monospace;
+            font-weight: lighter;
+            color: #333333;
+          }
+
+          .upperCard {
+            align-items: flex-end;
+            border-bottom: 0.5px solid whitesmoke;
+            border-top-left-radius: 3px;
+            border-top-right-radius: 3px;
+          }
+          .upperCard span {
+            transform: translateY(50%);
+          }
+
+          .lowerCard {
+            align-items: flex-start;
+            border-top: 0.5px solid whitesmoke;
+            border-bottom-left-radius: 3px;
+            border-bottom-right-radius: 3px;
+          }
+          .lowerCard span {
+            transform: translateY(-50%);
+          }
+
+          .flipCard {
+            display: flex;
+            justify-content: center;
+            position: absolute;
+            left: 0;
+            width: 100%;
+            height: 50%;
+            overflow: hidden;
+            backface-visibility: hidden;
+          }
+          .flipCard span {
+            font-family: "Droid Sans Mono", monospace;
+            font-size: 5em;
+            font-weight: lighter;
+            color: #333333;
+          }
+          .flipCard.unfold {
+            top: 50%;
+            align-items: flex-start;
+            transform-origin: 50% 0%;
             transform: rotateX(180deg);
+            background-color: white;
+            border-bottom-left-radius: 3px;
+            border-bottom-right-radius: 3px;
+            border: 0.5px solid whitesmoke;
+            border-top: 0.5px solid whitesmoke;
           }
-          100% {
+          .flipCard.unfold span {
+            transform: translateY(-50%);
+          }
+          .flipCard.fold {
+            top: 0%;
+            align-items: flex-end;
+            transform-origin: 50% 100%;
             transform: rotateX(0deg);
+            background-color: white;
+            border-top-left-radius: 3px;
+            border-top-right-radius: 3px;
+            border: 0.5px solid whitesmoke;
+            border-bottom: 0.5px solid whitesmoke;
           }
-        }
-      `}</style>
+          .flipCard.fold span {
+            transform: translateY(50%);
+          }
+
+          .fold {
+            animation: fold 0.6s cubic-bezier(0.455, 0.03, 0.515, 0.955) 0s 1
+              normal forwards;
+            transform-style: preserve-3d;
+          }
+
+          .unfold {
+            animation: unfold 0.6s cubic-bezier(0.455, 0.03, 0.515, 0.955) 0s 1
+              normal forwards;
+            transform-style: preserve-3d;
+          }
+
+          @keyframes fold {
+            0% {
+              transform: rotateX(0deg);
+            }
+            100% {
+              transform: rotateX(-180deg);
+            }
+          }
+
+          @keyframes unfold {
+            0% {
+              transform: rotateX(180deg);
+            }
+            100% {
+              transform: rotateX(0deg);
+            }
+          }
+        `}</style>
+      </div>
     </div>
   );
 };
