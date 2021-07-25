016ed64bf650b809b9e05387e585feae4abf09ce
Dave Waterman <waterman_david@bah.com>
2021-07-24 20:16:11 -0400

    added content authoring to splash page

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,19 @@
+import { fetchEntry } from "@utils/contentful";
 import Link from "next/link";
 import Layout from "../components/Layout";
 
-const Splash = () => {
+const Splash = ({ content }) => {
   return (
     <Layout>
       <div className="container bg-black min-h-screen">
         <section className="bg-black flex flex-col justify-center">
-          <p className="text-center text-white mt-6 text-2xl">Body text</p>
+          <p className="text-center text-white mt-6 text-2xl">
+            {content.bodyText}
+          </p>
           <div className="text-center m-auto mt-6">
             <Link href="/home">
               <button className="text-white w-auto cursor-pointer leading-none px-3 py-1 border border-solid rounded bg-transparent block outline-none focus:outline-none hover:bg-gray-500">
-                Click me for a good time!
+                {content.buttonText}
               </button>
             </Link>
           </div>
@@ -19,5 +22,13 @@ const Splash = () => {
     </Layout>
   );
 };
-
+export async function getStaticProps() {
+  const res = await fetchEntry("2DwrFcplmFjr2dXLmBFOrK");
+  const content = res?.fields;
+  return {
+    props: {
+      content,
+    },
+  };
+}
 export default Splash;
